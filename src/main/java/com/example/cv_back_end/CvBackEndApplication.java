3e15c71fabb1d89420a38a169ecb5c76c7994bd2
lenovo-legion <WZLSXJFMSFQZSN3>
2023-03-23 11:59:44 +0800

    init

---
+++ src/main/java/com/example/cv_back_end/CvBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.cv_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CvBackEndApplication.class, args);
+    }
+
+}
